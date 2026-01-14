efb1eb5335f144ece0745b4db9a1c3b84b6b7eaa
wisdomrainmusic <alisverissepetiniz4601@gmail.com>
2026-01-14 07:44:02 +0300

    Add files via upload

--- inc/tools/tools-loader.php
+++ inc/tools/tools-loader.php
@@ -1,8 +1,13 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+if ( ! defined( 'ABSPATH' ) ) { exit; }
 
+define( 'HMPRO_CATEGORY_IMPORTER_EMBEDDED', true );
 require_once HMPRO_PATH . '/inc/tools/category-importer/category-importer.php';
 
-hmpro_get_category_importer();
+add_action( 'admin_init', function () {
+    if ( function_exists( 'hmpro_get_category_importer' ) ) {
+        hmpro_get_category_importer();
+    } elseif ( class_exists( 'HM_Pro_Category_Importer' ) ) {
+        new HM_Pro_Category_Importer( false );
+    }
+}, 20 );
