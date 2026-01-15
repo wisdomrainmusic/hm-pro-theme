cd10c4e26f9b2d160e4ad1457d19f5dfd81f10a3
wisdomrainmusic <alisverissepetiniz4601@gmail.com>
2026-01-15 10:44:37 +0300

    Add files via upload

--- inc/core/enqueue.php
+++ inc/core/enqueue.php
@@ -11,13 +11,4 @@ add_action( 'wp_enqueue_scripts', function () {
 		[],
 		HMPRO_VERSION
 	);
-
-	// Frontend Mega Menu helpers (position + "Show more" behaviour).
-	wp_enqueue_script(
-		'hmpro-mega-menu',
-		get_template_directory_uri() . '/assets/js/mega-menu.js',
-		[],
-		HMPRO_VERSION,
-		true
-	);
 } );
